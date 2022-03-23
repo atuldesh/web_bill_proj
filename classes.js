3dea272c05a33d30dbf480111cde6d2d025ab30a
atuldesh <atul.desh@gmail.com>
2022-03-23 13:06:16 +0530

    mar23_2

---
+++ classes.js
@@ -0,0 +1,31 @@
+class BillItem{
+    item;
+    qty;
+    rate;
+    constructor(item,qty,rate){
+        this.item = item;
+        this.qty = qty;
+        this.rate = rate; 
+    }
+    amount() { 
+        return this.qty * this.rate;
+    }
+};
+class Bill{
+    bdate;
+    ttype;
+    cname;
+    billItems = [];
+    constructor(bdate,ttype,cname){
+        this.bdate = bdate;
+        this.ttype = ttype;
+        this.cname = cname;
+    };
+    billAmount() {
+        let tot=0;
+        for(i=0;i<this.billItems.length;i++){
+            tot = tot + this.billItems[i].amount();
+        }
+        return tot;
+    }
+};
